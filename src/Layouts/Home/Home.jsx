466b0707093caf346673b0420b2d1516041f5280
hriday <alauddinhriday@gmail.com>
2025-02-19 19:13:05 +0600

    added animation

--- src/Layouts/Home/Home.jsx
+++ src/Layouts/Home/Home.jsx
@@ -2,9 +2,9 @@
 import Banner from "../../Components/Banner/Banner";
 import { Helmet } from "react-helmet-async";
 import Categories from "../../Components/Categories/Categories";
-import { Link } from "react-router-dom";
 import DiscountProduct from "../../Components/DiscountProduct/DiscountProduct";
 import Accordion from "../../Components/Accordion/Accordion";
+import { motion } from "motion/react"
 
 const Home = () => {
     return (
@@ -12,14 +12,27 @@ const Home = () => {
             <Helmet>
                 <title>MediTrust | Home</title>
             </Helmet>
-            <Banner/>
+            <Banner />
             <div className="py-6 px-4">
-                <h1 className="text-center text-2xl font-bold mb-2">Your Trusted Partner in Health and Wellness.</h1>
-                <p className="text-slate-500 text-center">MediTrustPharma is a trusted online platform for authentic medicines and healthcare products. With a wide range of offerings, easy prescription uploads, and fast doorstep delivery, we simplify your healthcare journey while ensuring quality and affordability.</p>
+                <motion.div
+                    initial={{ x: 0, y: -120 }}
+                    whileInView={{ x: 0, y: 0 }}
+                    transition={{ duration: 0.9 }}
+                >
+                    <h1 className="text-center text-2xl font-bold mb-2">Your Trusted Partner in Health and Wellness.</h1>
+                </motion.div>
+
+                <motion.div
+                    initial={{ x: 0, y: 120 }}
+                    whileInView={{ x: 0, y: 0 }}
+                    transition={{ duration: 0.9 }}
+                >
+                    <p className="text-slate-500 text-center">MediTrustPharma is a trusted online platform for authentic medicines and healthcare products. With a wide range of offerings, easy prescription uploads, and fast doorstep delivery, we simplify your healthcare journey while ensuring quality and affordability.</p>
+                </motion.div>
             </div>
-            <Categories/>
-            <DiscountProduct/>
-            <Accordion/>
+            <Categories />
+            <DiscountProduct />
+            <Accordion />
         </div>
     );
 };
